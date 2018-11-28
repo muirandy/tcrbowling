e7e09e5a5d645c0a7ed11d0ac5109193101cc9de
Andy Muir <andy.muir@aimyourtechnology.com>
2018-11-28 11:20:59 +0000

    working

---
+++ src/test/java/com/github/muirandy/tcr/bowling/TcrTest.java
@@ -0,0 +1,14 @@
+package com.github.muirandy.tcr.bowling;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TcrTest {
+
+    @Test
+    public void t() {
+        assertTrue(true);
+    }
+
+}
