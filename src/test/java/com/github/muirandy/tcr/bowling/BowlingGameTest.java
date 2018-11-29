7379a2c3e3659621f215c6f93655c1fdc98d7ef3
Andy Muir <andy.muir@aimyourtechnology.com>
2018-11-29 17:41:31 +0000

    working

--- src/test/java/com/github/muirandy/tcr/bowling/BowlingGameTest.java
+++ src/test/java/com/github/muirandy/tcr/bowling/BowlingGameTest.java
@@ -8,15 +8,21 @@ public class BowlingGameTest {
 
     private BowlingGame game = new BowlingGame();
 
+    private void roll(int numberOfRolls, int pinsPerRoll) {
+        for (int i = 0; i< numberOfRolls; i++)
+            game.roll(pinsPerRoll);
+    }
+
     @Test
     public void gutterGame() {
         roll(20, 0);
         assertEquals(0, game.score());
     }
 
-    private void roll(int numberOfRolls, int pinsPerRoll) {
-        for (int i = 0; i< numberOfRolls; i++)
-            game.roll(pinsPerRoll);
+    @Test
+    public void allOnes() {
+        roll(20, 1);
+        assertEquals(20, game.score());
     }
 
 }
