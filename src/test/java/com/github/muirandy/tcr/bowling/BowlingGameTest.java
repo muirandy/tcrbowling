e332996f5d0c08c8bdf7f849020d13f284329785
Andy Muir <andy.muir@aimyourtechnology.com>
2018-11-29 17:37:08 +0000

    working

--- src/test/java/com/github/muirandy/tcr/bowling/BowlingGameTest.java
+++ src/test/java/com/github/muirandy/tcr/bowling/BowlingGameTest.java
@@ -2,13 +2,14 @@ package com.github.muirandy.tcr.bowling;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BowlingGameTest {
 
     @Test
-    public void t() {
-        assertTrue(true);
+    public void gutterGame() {
+        BowlingGame game = new BowlingGame();
+        assertEquals(0, game.score());
     }
 
 }
